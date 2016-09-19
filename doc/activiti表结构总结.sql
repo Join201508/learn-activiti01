45b3cedf12cf8bc40ff5a318a4fdc9837c86f326
join <join09@qq.com>
2016-09-19 23:00:17 +0800

    增加表结构说明总结

---
+++ doc/activiti表结构总结.sql
@@ -0,0 +1,14 @@
+/*初始化*/
+SELECT * FROM `act_ge_property`; #主键生成策略等属性
+
+/*流程部署*/
+SELECT * FROM act_re_deployment; #部署对象表：部署一次增加一条记录
+SELECT * FROM act_re_procdef; #流程定义表：版本信息等，当多次部署的流程的key相同时，在此表中保存对应的版本信息(version_字段)
+SELECT * FROM act_ge_bytearray; #资源文件表
+
+/*流程实例：act_ru开头的表*/
+SELECT * FROM act_ru_execution; #正在执行的对象
+SELECT * FROM act_hi_procinst;  #流程实例历史表，流程没有结束时end_time_为空
+SELECT * FROM act_ru_task;      #正在执行的任务表（只有节点是UserTask时，该表中才有数据）
+SELECT * FROM act_hi_taskinst;  #任务历史表（只有节点是UserTask时，该表中才有数据）
+SELECT * FROM act_hi_actinst;   #所有活动节点的历史表
